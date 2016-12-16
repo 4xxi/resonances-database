1d81f45339257098f21adb7e1e9d4d79896510d7
antlord <antlord92@gmail.com>
2016-12-17 00:13:02 +0800

    Added PHA

--- asteroids.sql
+++ asteroids.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc9ac64254eac29c5823f14ba6b444348d28a2ade28c4c974437e1955603fe72
-size 115451257
+oid sha256:dc80092a57726e6bbe2c1fad1eb3cde47adf509aec1c5d5b648da6a64f077b03
+size 115517748
