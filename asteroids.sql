f6d12b1c2e0ffa0b7093d5e615dcde1389ead55e
antlord <antlord92@gmail.com>
2016-12-21 22:43:32 +0800

    added all two body librations

--- asteroids.sql
+++ asteroids.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc80092a57726e6bbe2c1fad1eb3cde47adf509aec1c5d5b648da6a64f077b03
-size 115517748
+oid sha256:36d3bf9c7c0ee634158635299f04b435b4016e191245146b158b173b4d0bec48
+size 115983090
